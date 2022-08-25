010cb5fa1053e5a4b11382575e4b32b58a9805ba
oja-daddy <you@example.com>
2022-08-25 22:51:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/affirmation/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.affirmation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmation.adapter.ItemAdapter
+import com.example.affirmation.data.Datasource
+import javax.sql.DataSource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myDataset = Datasource().loadAffirmation()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+
+    }
+}
